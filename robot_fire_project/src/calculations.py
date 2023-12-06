8d1ec3a7cb2207d1012d666fece626d4b3e92263
antjimmad <antjimmad@alum.us.es>
2023-12-06 20:25:17 +0100

    Remove robot project folder

--- robot_fire_project/src/calculations.py
+++
@@ -1,7 +0,0 @@
-def calculate_position(obj):
-    # Exemple de fonction de calcul simplifiée
-    x, y, w, h = obj["coordinates"]
-    azimuth = x * 0.1  # Exemple de calcul
-    elevation = y * 0.1
-    distance = (w + h) * 0.5
-    return {"azimuth": azimuth, "elevation": elevation, "distance": distance}
