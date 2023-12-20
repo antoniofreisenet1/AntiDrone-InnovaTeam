85759077a5ec569b030cb7e2e8b99692e8399d32
lema60879 <151541748+lema60879@users.noreply.github.com>
2023-12-20 19:51:49 +0100

    maj essai

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from ev3dev.ev3 import *
 from time       import sleep
 
 m1 = LargeMotor('outB')
-#m2 = LargeMotor('outC')
+m2 = LargeMotor('outC')
 assert m1.connected, "Connecter un large motor sur outB"
 
 m1.run_forever(speed_sp = 200)
 
 sleep(2)
 m1.run_forever(speed_sp = 0)
-#m2.run_forever(speed_sp = 200)
-#m2.run_forever(speed_sp = 0)
+m2.run_forever(speed_sp = 200)
+m2.run_forever(speed_sp = 0)
 
 sleep(1)
